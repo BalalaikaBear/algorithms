0f92f344d95613c952765ee8b25753a7ece4633c
BalalaikaBear <eightes@yandex.ru>
2025-01-26 16:26:54 +0300

    None

--- grokking_algorithms_se/chapter4_fast_sorting/recursion.py
+++ grokking_algorithms_se/chapter4_fast_sorting/recursion.py
@@ -1,26 +1,26 @@
-def summ(_list: list) -> int:
+def summ(array: list) -> int:
     """Рекурсивная функция, суммирующая элементы в списке"""
-    if not _list:  # if _list == []:
+    if not array:  # if _list == []:
         return 0
     else:
-        return _list[0] + summ(_list[1:])
+        return array[0] + summ(array[1:])
 
 
-def count(_list: list) -> int:
+def count(array: list) -> int:
     """Рекурсивная функция, возвращающая количество элементов в списке"""
-    if not _list:
+    if not array:
         return 0
     else:
-        return 1 + count(_list[1:])
+        return 1 + count(array[1:])
 
 
-def maximum(_list: list) -> int | None:
+def maximum(array: list) -> int | None:
     """Рекурсивная функция, возвращающая максимальное """
-    if not _list:
+    if not array:
         return None
     else:
-        _max = _list[0]
-        _max_in_list = maximum(_list[1:])
+        _max = array[0]
+        _max_in_list = maximum(array[1:])
         if _max_in_list and _max_in_list > _max:
             _max = _max_in_list
         return _max
@@ -42,8 +42,20 @@ def bin_search(sorted_list: list, number_to_find: int) -> True:
             return bin_search(sorted_list[:len(sorted_list) // 2], number_to_find)
 
 
+def quick_sort(array: list) -> list:
+    """Рекурсивная функция сортировки списка (быстрая сортировка)"""
+    if len(array) < 2:
+        return array
+    else:
+        pivot = array[0]
+        less = [i for i in array[1:] if i < pivot]
+        greater = [i for i in array[1:] if i > pivot]
+        return quick_sort(less) + [pivot] + quick_sort(greater)
+
+
 if __name__ == '__main__':
     print(summ([1, 2, 3, 10, -2]))
     print(count([1, 3, 4, 1]))
     print(maximum([-3, -2, -1]))
     print(bin_search([1, 2, 4, 6, 11, 13, 58, 192], 192))
+    print(quick_sort([4, 12, -48, 0, 1]))
