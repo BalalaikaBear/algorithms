4cfaabbfea862e1ceb249b74e83cb81c492ebb6f
BalalaikaBear <eightes@yandex.ru>
2025-02-13 22:31:26 +0300

    Deleting functionality and length calculation have been added to the AVLTree

--- trees.py
+++ trees.py
@@ -3,6 +3,7 @@ from typing import Optional, Any, Iterable, Callable
 
 
 class Node:
+    """Узел для АВЛ-дерева"""
     def __init__(self, value: Any):
         self.data: Any = value
         self.left: Optional["Node"] = None
@@ -17,6 +18,7 @@ class Node:
 
 
 class AVLTree:
+    """Сбалансированное по высоте двоичное дерево поиска"""
     def __init__(self,
                  data: Optional[Iterable] = None,
                  *,
@@ -24,6 +26,7 @@ class AVLTree:
                  rewrite: bool = False) -> None:
         self.root: Optional[Node] = None  # корень дерева
         self.rewrite = rewrite
+        self._len: int = 0
 
     def insert(self, array: Iterable) -> None:
         """Добавление последовательности в дерево.
@@ -45,11 +48,12 @@ class AVLTree:
 
         :param node: Проверяемый узел.
         :param value: Добавляемый объект.
-        :return: Возвращает корень дерева.
+        :return: Возвращает корневой узел дерева.
         """
         if not node:
             new_node = Node(value)
             logging.info("Создание узла {}".format(new_node))
+            self._len += 1
             return new_node
 
         if self.rewrite and value == node.data:
@@ -66,8 +70,8 @@ class AVLTree:
     def _balance(self, node: Node) -> Node:
         """Балансировка дерева
 
-        :param node: Верхний узел дерева.
-        :return: Возвращает новый верхний узел.
+        :param node: Узел дерева.
+        :return: Возвращает узел ``node`` со сбалансированными ветвями.
         """
         self._update_height(node)
         bf = self._balance_factor(node)
@@ -120,7 +124,7 @@ class AVLTree:
         """Левый поворот ветви дерева
 
         :param parent: Верхний узел дерева.
-        :return: Возвращает новый верхний узел.
+        :return: Возвращает новый верхний узел ``node``.
         """
         new_parent: Node = parent.right
         sub_branch: Optional[Node] = new_parent.left
@@ -137,7 +141,7 @@ class AVLTree:
         """Правый поворот ветви дерева
 
         :param parent: Верхний узел дерева.
-        :return: Возвращает новый верхний узел.
+        :return: Возвращает новый верхний узел ``node``.
         """
         new_parent: Node = parent.left
         sub_branch: Optional[Node] = new_parent.right
@@ -149,6 +153,65 @@ class AVLTree:
         self._update_height(new_parent)
 
         return new_parent
+
+    def delete(self, value: Any) -> None:
+        """Удалить объект из дерева.
+
+        :param value: Объект для удаления из дерева
+        """
+        self.root = self._delete(self.root, value)
+
+    def _delete(self, node: Optional[Node], value: Any) -> Node:
+        """Рекурсивная функция удаления узла из дерева по введенному значению.
+
+        :param node: Проверяемый узел.
+        :param value: Значение для удаления из дерева.
+        :return: Возвращает перезаписанный (корневой) узел ``node``.
+        """
+        if self._find_value(node, value):
+            # узел с одним ребенком
+            if not node.left:
+                return node.right
+            elif not node.right:
+                return node.left
+
+            # замена найденного значения минимальным значением из правого поддерева
+            new_node = self._min_value_node(node.right)
+            node.data = new_node.data
+            logging.info("Удаление значения {0}. Замена {0} на {1}".format(value, node.data))
+            node.right = self._delete(node.right, node.data)
+            self._len -= 1
+
+        return self._balance(node)
+
+    def _find_value(self, node: Optional[Node], value: Any) -> Node:
+        """Поиск объекта/значения в дереве.
+
+        :param node: Узел, в котором осуществляется поиск.
+        :param value: Объект/значение для поиска.
+        :raise ValueError: Значение не найдено.
+        :return: Узел ``node`` с необходимым значением.
+        """
+        if node is None:
+            raise ValueError(f'value {value} is not in {self}')
+
+        if value < node.data:
+            node.left = self._delete(node.left, value)
+        elif value > node.data:
+            node.right = self._delete(node.right, value)
+        else:
+            logging.info("Значение {} найдено".format(value))
+            return node
+
+    def _min_value_node(self, node: Node) -> Node:
+        """Поиск минимального значения.
+
+        :param node: Узел начала поиска.
+        :return: Узел ``node`` с минимальным значением.
+        """
+        while node.left:
+            node = node.left
+        return node
 
     def print(self) -> None:
         """Удобное отображение дерева в командной строке"""
@@ -162,6 +225,8 @@ class AVLTree:
                 self._print_recursion(node.left, depth + 1, '├-- \033[0mL: ')
                 self._print_recursion(node.right, depth + 1, '├-- \033[0mR: ')
 
+    def __len__(self) -> int:
+        return self._len
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO,
@@ -171,8 +236,12 @@ if __name__ == '__main__':
                         format='LOGGING_LEVEL: %(levelno)s - %(message)s')
 
     r = AVLTree(rewrite=True)
-    list_ = [10, 20, 3, 7, 8, 9, 1, 1, 1, 1]
+    list_ = [10, 20, 3, 7, 8, 9, 1, 6, 1, 1]
 
     r.insert(list_)
     r.print()
+    print(len(r))
     print(r.root)
+    r.delete(8)
+    r.print()
+    print(len(r))
